37f28027d13b39d34bce176ac7ffcd9f415f1540
rohit0110 <54764168+rohit0110@users.noreply.github.com>
2020-12-14 01:30:48 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api
+from flask_pagedown import PageDown
+import os
+
+api = Api()
+pagedown = PageDown()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.urandom(24)
+    with app.app_context():
+        # extensions
+        api.init_app(app)
+        pagedown.init_app(app)
+
+        from core import core
+        app.register_blueprint(core)
+
+        return app
